5aa9dfed26a5839ffa2c33833e221fc831a3aac1
Chrisdaman417 <74510908+Chrisdaman417@users.noreply.github.com>
2020-11-17 06:51:50 -0500

    Snake moves, Snake grows, Apple Spawns

--- Snek.py
+++ Snek.py
@@ -1,17 +1,98 @@
 import pygame
-#from pygame.locals import *
+import random
 
+class segment:
+    def __init__(self):
+        self.width = 10
+        self.height = 10
+        self.x = -10
+        self.y = -10
+        self.dir = 0
+        self.full = False
+        self.Surface = pygame.Surface((self.width, self.height))
+
+    def gen(self):
+        self.Surface.fill((255,0,0))
+        self.Surface.convert()
+        theApp.screen.blit(self.Surface, (self.x, self.y))
+
+class apple:
+    def __init__(self):
+        self.width = 10
+        self.height = 10
+        self.x = round(random.randint(0,theApp.width-10)/10)*10
+        self.y = round(random.randint(0,theApp.height-10)/10)*10
+        self.Surface = pygame.Surface((self.width, self.height))
+
+    def placeFood(self):
+        self.Surface.fill((255,255,0))
+        self.Surface.convert()
+        theApp.screen.blit(self.Surface, (self.x, self.y))
+
+
+class snake:
+    def __init__(self):
+        self.body = []
+        self.moving = False
+        self.body.append(segment())
+        self.body[0].x = 20
+        self.body[0].y = 360
+        self.body.append(segment())
+        self.body[0].x = 10
+        self.body[0].y = 360
+        self.body.append(segment())
+        self.body[0].x = 0
+        self.body[0].y = 360
+
+    def tailMe(self):
+        self.body[0].x = self.body[1].x
+        self.body[0].y = self.body[1].y
+        self.body[0].dir = self.body[1].dir
+
+    def move(self):
+        if self.body[0].dir == 0:
+            self.body.insert(0,segment())
+            self.tailMe()
+            self.body.pop()
+            self.body[0].x -= 10
+
+        elif self.body[0].dir == 1:
+            self.body.insert(0,segment())
+            self.tailMe()
+            self.body.pop()
+            self.body[0].y -= 10
+
+        elif self.body[0].dir == 2:
+            self.body.insert(0,segment())
+            self.tailMe()
+            self.body.pop()
+            self.body[0].x += 10
+
+        elif self.body[0].dir == 3:
+            self.body.insert(0,segment())
+            self.tailMe()
+            self.body.pop()
+            self.body[0].y += 10
+
+        for i in range(len(self.body)):
+            self.body[i].gen()
+
+    def grow(self):
+        self.body.append(segment())
 class App:
     def __init__(self):
         self._running = True
         self.screen = None
+        self.nofood = True
         self.gameTick = 0
-        self.size = self.width , self.height = 1280 , 720
+        self.size = self.width , self.height = 600 , 600
 
     def on_init(self):
         pygame.init()
         self.screen = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
-        self.background = pygame.Surface((int(self.width/2),int(self.height/2)))
+        self.background = pygame.Surface((self.width,self.height))
+        self.background.fill((0,0,0))
+        self.background.convert()
         self._running = True
 
     def on_event(self, event):
@@ -19,34 +100,30 @@ class App:
             self._running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                _running = False
-            if event.key == pygame.K_RETURN:
-                self.background.fill((255,255,255))
-                self.background.convert()
-                self.screen.blit(self.background, (int(self.width / 2 / 2),int(self.height / 2 / 2)))
-            if event.key == pygame.K_SPACE:
-                self.background.fill((0,255,255))
-                self.background.convert()
-                self.screen.blit(self.background, (int(self.width / 2 / 2),int(self.height / 2 / 2)))
+                self._running = False
             if event.key == pygame.K_UP:
-                box.dir = 1
-                box.moving = True
+                snekington.body[0].dir = 1
+                snekington.moving = True
             if event.key == pygame.K_LEFT:
-                box.dir = 0
-                box.moving = True
+                snekington.body[0].dir = 0
+                snekington.moving = True
             if event.key == pygame.K_RIGHT:
-                box.dir = 2
-                box.moving = True
+                snekington.body[0].dir = 2
+                snekington.moving = True
             if event.key == pygame.K_DOWN:
-                box.dir = 3
-                box.moving = True
+                snekington.body[0].dir = 3
+                snekington.moving = True
+            if event.key == pygame.K_SPACE:
+                snekington.grow()
 
     def on_loop(self):
-        if box.moving == True:
+        if self.nofood == True:
+            food1.placeFood()
+            self.nofood == False
+        if snekington.moving == True:
             if pygame.time.get_ticks() >= self.gameTick + 250:
-                box.tailMe(box2)
-                box.move()
-                box2.gen()
+                self.screen.blit(self.background,(0,0))
+                snekington.move()
                 self.gameTick = pygame.time.get_ticks()
     def on_render(self):
         pygame.display.update()
@@ -64,59 +141,8 @@ class App:
             self.on_render()
         self.on_cleanup()
 
-class segment:
-    def __init__(self):
-        self.moving = False
-        self.width = 10
-        self.height = 10
-        self.x = 0
-        self.y = 0
-        self.dir = 0
-        self.Surface = pygame.Surface((self.width, self.height))
-
-    def move(self):
-        if self.dir == 0:
-            self.Surface.fill((0,0,0))
-            theApp.screen.blit(self.Surface, (self.x, self.y))
-            self.Surface.fill((255,0,0))
-            self.x -= 10
-            self.Surface.convert()
-            theApp.screen.blit(box.Surface, (self.x, self.y))
-        if self.dir == 1:
-            self.Surface.fill((0,0,0))
-            theApp.screen.blit(box.Surface, (self.x, self.y))
-            self.Surface.fill((255,0,0))
-            self.y -= 10
-            self.Surface.convert()
-            theApp.screen.blit(self.Surface, (self.x, self.y))
-        if self.dir == 2:
-            self.Surface.fill((0,0,0))
-            theApp.screen.blit(self.Surface, (self.x, self.y))
-            self.Surface.fill((255,0,0))
-            self.x += 10
-            self.Surface.convert()
-            theApp.screen.blit(self.Surface, (self.x, self.y))
-        if self.dir == 3:
-            self.Surface.fill((0,0,0))
-            theApp.screen.blit(self.Surface, (self.x, self.y))
-            self.Surface.fill((255,0,0))
-            self.y += 10
-            self.Surface.convert()
-            theApp.screen.blit(self.Surface, (self.x, self.y))
-
-    def tailMe(self,segment):
-        segment.x = self.x
-        segment.y = self.y
-        segment.dir = self.dir
-        self.gen()
-
-    def gen(self):
-        self.Surface.fill((255,0,0))
-        self.Surface.convert()
-        theApp.screen.blit(box.Surface, (box.x, box.y))
-
 if __name__ == "__main__":
     theApp = App()
-    box = segment()
-    box2 = segment()
+    snekington = snake()
+    food1 = apple()
     theApp.on_execute()
