0eaacb29aa2386bfd2b47e120acffaa8ae081e02
Chrisdaman417 <74510908+Chrisdaman417@users.noreply.github.com>
2020-11-17 21:41:57 -0500

    Functional 1.0

--- Snek.py
+++ Snek.py
@@ -1,5 +1,22 @@
 import pygame
+from pygame.locals import *
 import random
+
+class apple:
+    def __init__(self):
+        self.width = 10
+        self.height = 10
+        self.on = 0
+        self.Surface = pygame.image.load('apple.png')
+
+    # Spawn apple in random location
+    def placeFood(self):
+        self.x = round(random.randint(0,theApp.width-10)/10)*10
+        self.y = round(random.randint(0,theApp.height-10)/10)*10
+
+    def genFood(self):
+        self.Surface.convert()
+        theApp.screen.blit(self.Surface, (self.x, self.y))
 
 class segment:
     def __init__(self):
@@ -8,6 +25,7 @@ class segment:
         self.x = -10
         self.y = -10
         self.dir = 2
+        self.dirold = 2
         self.Surface = pygame.Surface((self.width, self.height))
 
     # Display Segment
@@ -15,24 +33,7 @@ class segment:
         if snekington.moving == True:
             self.Surface.fill((0,255,0))
         else:
-            self.Surface.fill((255,0,255))
-        self.Surface.convert()
-        theApp.screen.blit(self.Surface, (self.x, self.y))
-
-class apple:
-    def __init__(self):
-        self.width = 10
-        self.height = 10
-        self.on = 0
-        self.Surface = pygame.Surface((self.width, self.height))
-
-    # Spawn apple in random location
-    def placeFood(self):
-        self.x = round(random.randint(0,theApp.width-10)/10)*10
-        self.y = round(random.randint(0,theApp.height-10)/10)*10
-
-    def genFood(self):
-        self.Surface.fill((255,0,0))
+            self.Surface.fill((63, 32, 0))
         self.Surface.convert()
         theApp.screen.blit(self.Surface, (self.x, self.y))
 
@@ -40,6 +41,7 @@ class snake:
     def __init__(self):
         self.body = []
         self.moving = True
+        self.didMove = False
         self.setSnake()
 
     def setSnake(self):
@@ -59,12 +61,17 @@ class snake:
         self.body[0].x = self.body[1].x
         self.body[0].y = self.body[1].y
         self.body[0].dir = self.body[1].dir
+        self.body[0].dirold = self.body[1].dirold
 
     # Update Position of the Snake
     def move(self):
 
         # Move Left
         if self.body[0].dir == 0:
+            if self.body[0].dirold == 2:
+                self.body[0].dir = 2
+                self.move()
+                return
             self.crash(food1)
             if self.moving == False:
                 self.genSnake()
@@ -76,6 +83,10 @@ class snake:
 
         # Move Up
         elif self.body[0].dir == 1:
+            if self.body[0].dirold == 3:
+                self.body[0].dir = 3
+                self.move()
+                return
             self.crash(food1)
             if self.moving == False:
                 self.genSnake()
@@ -87,6 +98,10 @@ class snake:
 
         # Move Right
         elif self.body[0].dir == 2:
+            if self.body[0].dirold == 0:
+                self.body[0].dir = 0
+                self.move()
+                return
             self.crash(food1)
             if self.moving == False:
                 self.genSnake()
@@ -98,6 +113,10 @@ class snake:
 
         # Move Down
         elif self.body[0].dir == 3:
+            if self.body[0].dirold == 1:
+                self.body[0].dir = 1
+                self.move()
+                return
             self.crash(food1)
             if self.moving == False:
                 self.genSnake()
@@ -107,6 +126,7 @@ class snake:
             self.body.pop()
             self.body[0].y += 10
         self.genSnake()
+        self.body[0].dirold = self.body[0].dir
 
     # Display all Segments in Snake Body
     def genSnake(self):
@@ -119,13 +139,12 @@ class snake:
     
     # Collision Detection
     def crash(self, apple):
-
         # Check move left
         if self.body[0].dir == 0:
             # Wall Check
             if (self.body[0].x-10 == -10):
-                    self.moving = False
-                    return
+                self.moving = False
+                return
             # Body Check
             for i in range(len(self.body)):
                 if (self.body[0].x-10 == self.body[i].x) and (self.body[0].y == self.body[i].y):
@@ -138,11 +157,11 @@ class snake:
                 return
 
         # Check move up
-        if self.body[0].dir == 1:
+        elif self.body[0].dir == 1:
             # Wall Check
             if (self.body[0].y-10 == -10):
-                    self.moving = False
-                    return
+                self.moving = False
+                return
             # Body Check
             for i in range(len(self.body)):
                 if (self.body[0].x == self.body[i].x) and (self.body[0].y-10 == self.body[i].y):
@@ -155,11 +174,11 @@ class snake:
                 return
 
         # Check move right
-        if self.body[0].dir == 2:
+        elif self.body[0].dir == 2:
             # Wall Check
             if (self.body[0].x+10 == theApp.width):
-                    self.moving = False
-                    return
+                self.moving = False
+                return
             # Body Check
             for i in range(len(self.body)):
                 if (self.body[0].x+10 == self.body[i].x) and (self.body[0].y == self.body[i].y):
@@ -172,11 +191,11 @@ class snake:
                 return  
 
         # Check move down
-        if self.body[0].dir == 3:
+        elif self.body[0].dir == 3:
             # Wall Check
             if (self.body[0].y+10 == theApp.height):
-                    self.moving = False
-                    return
+                self.moving = False
+                return
             # Body Check
             for i in range(len(self.body)):
                 if (self.body[0].x == self.body[i].x) and (self.body[0].y+10 == self.body[i].y):
@@ -190,7 +209,8 @@ class snake:
 
 class App:
     def __init__(self):
-        pygame.display.set_caption("Snekinton's big Journey")
+        pygame.init()
+        pygame.display.set_caption("Snekington's big Journey")
         self._running = True
         self.screen = None
         self.gameTick = 0
@@ -199,14 +219,13 @@ class App:
         self.size = self.height , self.width
 
     def on_init(self):
-        pygame.init()
         self.screen = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self.background = pygame.image.load('background.png')
         self.background.convert()
         self._running = True
 
     def resetGame(self):
-        snekington.body = []
+        snekington.body.clear()
         snekington.setSnake()
         snekington.moving = True
         food1.placeFood()
@@ -218,40 +237,45 @@ class App:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 self._running = False
-            if event.key == pygame.K_UP:
+            elif event.key == pygame.K_UP:
                 if snekington.body[0].dir == 3:
-                    pass
+                    return
                 else:
                     snekington.body[0].dir = 1
-                    snekington.moving = True
-            if event.key == pygame.K_LEFT:
+            elif event.key == pygame.K_LEFT:
                 if snekington.body[0].dir == 2:
-                    pass
+                    return
                 else:
                     snekington.body[0].dir = 0
-                    snekington.moving = True        
-            if event.key == pygame.K_RIGHT:
+            elif event.key == pygame.K_RIGHT:
                 if snekington.body[0].dir == 0:
-                    pass
+                    return
                 else:
                     snekington.body[0].dir = 2
-                    snekington.moving = True
-            if event.key == pygame.K_DOWN:
+            elif event.key == pygame.K_DOWN:
                 if snekington.body[0].dir == 1:
-                    pass
+                    return
                 else:
                     snekington.body[0].dir = 3
-                    snekington.moving = True
+          
 
     # Calculations Block
     def on_loop(self):
+
         # End State Check
         if snekington.moving == False:
-            self.check = pygame.event.wait()
-            if self.check.key == pygame.K_ESCAPE:
-                self._running = False
-            if self.check.key == pygame.K_SPACE:
-                self.resetGame()
+            passEnd = False
+            while not passEnd:
+                self.check = pygame.event.wait(5000)
+                if self.check.type == KEYDOWN:
+                    if self.check.key == pygame.K_ESCAPE:
+                        self._running = False
+                        passEnd = True
+                    elif self.check.key == pygame.K_SPACE:
+                        self.resetGame()
+                        passEnd = True
+                    else:
+                        pass
         
         # is game valid
         if snekington.moving == True:
@@ -288,6 +312,7 @@ class App:
             self._running = False
 
         while( self._running ):
+            pygame.event.set_allowed(pygame.KEYDOWN)
             for event in pygame.event.get():
                 self.on_event(event)
             self.on_loop()
